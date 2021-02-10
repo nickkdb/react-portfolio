dd801a762ba27ac932d3c8340023e6e74c0e9655
wutango <nickkdb@gmail.com>
2021-02-10 15:38:51 -0500

    minor edits to comments

--- portfolio-app/src/pages/about.js
+++ portfolio-app/src/pages/about.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
-// import '../css/defaults.css';
 import Section1 from '../components/about/section1';
 import Section2 from '../components/about/section2';
 
